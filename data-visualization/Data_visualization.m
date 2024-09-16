f9683b777023f8030ad98c01c34d8e328aee1e43
jookare <joonakareinen@gmail.com>
2024-09-16 10:56:30 +0300

    Add images

--- data-visualization/Data_visualization.m
+++ data-visualization/Data_visualization.m
@@ -1,6 +1,6 @@
 % ADAML - Project work
 % Joona Kareinen
-%%
+%% Load data & preprocess
 clc
 close all 
 clearvars
@@ -19,7 +19,7 @@ for i = 1:4
     train_data{2, i} = Vars_final;
     
     % Test
-    [Test, Vars_final] = Data_preprocess(Test);
+    % [Test, Vars_final] = Data_preprocess(Test);
     test_data{1, i} = Test;
     test_data{2, i} = Vars_final;
 
@@ -37,19 +37,42 @@ for i = 1:4
     vars = test_data{2, i};
 
     % Do not take Unit and Time into account here
-    boxplot(data(:, 3:end), vars(3:end))
-    title("Train FD00"+num2str(i)+" (normalized)")
+    boxplot(data, vars)
+    title("Train FD00"+num2str(i))
 end
 
 %% Visualize testing data
-close all
+% close all
 for i = 1:4
     figure
     data = test_data{1, i};
     vars = test_data{2, i};
 
     % Do not take Unit and Time into account here
-    boxplot(data(:, 3:end), vars(3:end))
-    title("Test FD00"+num2str(i)+" (normalized)")
+    boxplot(data, vars)
+    title("Test FD00"+num2str(i))
 end
+
+%% Plot as a time series
+close all; figure
+data = train_data{1, 2};
+unit = 50;
+data = data(data(:,1) == unit, :);
+
+
+plot(data(:,2), data(:, 3:end), '.-')
+title("Train FD002 - unit "+num2str(unit))
+xlabel("Time (cycles)")
+axis tight
+% legend(vars(3:end))
+
+figure
+data = train_data{1, 1};
+data = data(data(:,1) == unit, :);
+
+plot(data(:,2), data(:, 3:end), '.-')
+title("Train FD001 - unit "+num2str(unit))
+xlabel("Time (cycles)")
+% legend(vars(3:end))
+
 
